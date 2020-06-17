81f08a5d065cedce84f721616c5b34f5148d9044
kuzeyfe <50907921+kuzeyfe@users.noreply.github.com>
2020-06-17 20:46:58 +0300

    Tamamlanmadı
    Tamamlanmadı

---
+++ Antikam/Controllers/UrunlerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Antikam.Controllers
+{
+    public class UrunlerController : Controller
+    {
+        // GET: Urunler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
